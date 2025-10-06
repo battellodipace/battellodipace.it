52eabadf07f70f93c4b1a5eb84606e15edd8bd01
Paolo Santori <code@paolosantori.it>
2025-10-06 20:48:50 +0200

    feat: add speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { HomePage } from './components/pages/HomePage';
@@ -90,6 +91,7 @@ export default function App() {
       </main>
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
